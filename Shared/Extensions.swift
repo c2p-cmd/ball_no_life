86e62e6bef2ea9f88ef3d56ea2b423be1b65271c
Sharan Thakur <sharandt19@live.com>
2024-01-27 22:57:38 +0530

    Adds extensions

--- Shared/Extensions.swift
+++ Shared/Extensions.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+extension String {
+    var isNumeric: Bool {
+        guard self.count > 0 else { return false }
+        let nums: Set<Character> = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        return Set(self).isSubset(of: nums)
+    }
+}
+
+
+extension Date {
+    var myFormat: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YYYY-MM-DD"
+        return formatter.string(from: self)
+    }
+}
